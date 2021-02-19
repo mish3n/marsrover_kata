390959aeb0b222ed94ae6fb840553e662f3a4256
Mihaela Encheva <mihaela.encheva@Mish-PC.localdomain>
2021-02-19 11:59:33 +0200

    Can rotate a bit more

--- marsRover.spec.js
+++ marsRover.spec.js
@@ -19,5 +19,6 @@ describe("Mars Rover", () => {
     it("should change direction correctly", () => {
         expect(execute("R")).toEqual("0:0:E");
         expect(execute("RR")).toEqual("0:0:S");
+        expect(execute("RRRR")).toEqual("0:0:N");
     });
 });
