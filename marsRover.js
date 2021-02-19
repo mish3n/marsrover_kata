6b02b4ae0a8d564d6c20616a45ba2d4c971a4503
Mihaela Encheva <mihaela.encheva@Mish-PC.localdomain>
2021-02-19 11:44:43 +0200

    added wrap around functionality

--- marsRover.js
+++ marsRover.js
@@ -1,15 +1,16 @@
 function execute(command) {
-
+    const CUBE_DIMENSIONS = 10;
+    
     let commands = command.split("");
     let y = 0;
     commands.forEach(item => {
         if (item === "M") {
             y++;
-        }  
+        }
     });
     
 
-    return `0:${y}:N`;
+    return `0:${y % CUBE_DIMENSIONS}:N`;
 }
 
 module.exports = { execute };
--- marsRover.spec.js
+++ marsRover.spec.js
@@ -8,5 +8,14 @@ describe("Mars Rover", () => {
     it("should move", () => {
         expect(execute("M")).toEqual("0:1:N");
         expect(execute("MMM")).toEqual("0:3:N");
+        expect(execute("MMMMM")).toEqual("0:5:N");
     });
+
+    it("should wrap around", () => {
+        expect(execute("MMMMMMMMMM")).toEqual("0:0:N");
+    });
+
+    // it("should change direction correctly", () => {
+    //     expect(execute("R")).toEqual("0:0:E");
+    // });
 });
