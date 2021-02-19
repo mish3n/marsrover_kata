c770ef323c7f418f14cbeb0a3121830970c0e796
Mihaela Encheva <mihaela.encheva@Mish-PC.localdomain>
2021-02-19 13:52:38 +0200

    refactoring

--- marsRover.js
+++ marsRover.js
@@ -1,37 +1,18 @@
 const DIRECTIONS = 'NESW';
+const CUBE_DIMENSIONS = 10;
 
-function execute(commands) {
-
-    let cmds = {"M": move, "R" : turnRight, "L": turnLeft};
-    
+function execute(input) {
+    let commands = {"M": move, "R" : turnRight, "L": turnLeft};
     let coords = { x: 0, y: 0, direction: 0 };
 
-    // [...commands].forEach(command => {
-    //     coords = cmds[command](coords);
-    // });
-
-    commands.split("").forEach(command => {
-        if (command === "M") {
-            coords = cmds[command](coords);
-        } else if (command === "R") {
-            coords = cmds[command](coords);
-        } else if (command === "L") {
-            coords.direction --;
-        }
-        
-        if(coords.direction < 0) {
-            coords.direction = 3;
-        }
+    [...input].forEach(command => {
+        coords = commands[command](coords);
     });
-    
-    let bearing = DIRECTIONS[coords.direction % DIRECTIONS.length];
 
-    return `${coords.x}:${coords.y}:${bearing}`;
+    return `${coords.x}:${coords.y}:${DIRECTIONS[coords.direction]}`;
 }
 
 function move(coords) {
-    const CUBE_DIMENSIONS = 10;
-
     const moves = [
         { x: 0, y: 1 },
         { x: 1, y: 0 },
@@ -39,43 +20,35 @@ function move(coords) {
         { x: -1, y: 0 },
     ];
 
-    let newCoords = {...coords};
-    newCoords.x += moves[coords.direction].x;
-    newCoords.y += moves[coords.direction].y;
+    coords.x += moves[coords.direction].x;
+    coords.y += moves[coords.direction].y;
 
-    if (newCoords.y < 0) {
-        newCoords.y = 9;
-    }
-    if (newCoords.x < 0) {
-        newCoords.x = 9;
-    }
-
-    newCoords.x = newCoords.x % CUBE_DIMENSIONS;
-    newCoords.y = newCoords.y % CUBE_DIMENSIONS;
-
-    return newCoords;
+    return keepCoordsWithinBoundries(coords);
 }
 
 function turnRight(coords) {
-    let newCoords = {...coords};
-    newCoords.direction += 1;
-    newCoords.direction %= DIRECTIONS.length;
+    coords.direction += 1;
+    return keepCoordsWithinBoundries(coords);
 }
 
-function turnLeft() {
-
+function turnLeft(coords) {
+    coords.direction -= 1;
+    return keepCoordsWithinBoundries(coords);
 }
 
+function keepCoordsWithinBoundries(coords) {
+    coords.x = keepNumberWithinBoundries(coords.x, 0, CUBE_DIMENSIONS);
+    coords.y = keepNumberWithinBoundries(coords.y, 0, CUBE_DIMENSIONS);
+    coords.direction = keepNumberWithinBoundries(coords.direction, 0, DIRECTIONS.length);
 
-function getMove(direction) {
-    const moves = [
-        { x: 0, y: 1 },
-        { x: 1, y: 0 },
-        { x: 0, y: -1 },
-        { x: -1, y: 0 },
-    ];
+    return coords;
+}
 
-    return moves[direction];
+function keepNumberWithinBoundries(number, min, max) {
+    if (number < min) {
+        number = max - 1;
+    }
+    return number % max;
 }
 
 module.exports = { execute };
