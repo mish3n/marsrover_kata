357aa1e36d4b80fa84f8a305b89fa84b8782ea60
Mihaela Encheva <mihaela.encheva@Mish-PC.localdomain>
2021-02-22 21:42:05 +0200

    added tests for square grid

--- marsRover.js
+++ marsRover.js
@@ -4,8 +4,10 @@ const SquareGrid = require("./SquareGrid").SquareGrid;
 const CommandModule = require("./CommandModule").CommandModule;
 
 function execute(input) {
+    let grid = new SquareGrid(10);
     let rover = new Rover(new Coords(0, 0), 0);
-    const commandModule = new CommandModule(new SquareGrid(10), rover, input);
+    
+    const commandModule = new CommandModule(grid, rover, input);
     commandModule.executePath(input);
     return rover.reportLocation();
 }
