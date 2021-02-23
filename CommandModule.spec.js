a3a2bd3e5c02981fc95000536527eb744fdfb254
Mihaela Encheva <mihaela.encheva@Mish-PC.localdomain>
2021-02-23 14:02:54 +0200

    test refactoring

--- CommandModule.spec.js
+++ CommandModule.spec.js
@@ -6,155 +6,101 @@ const CommandModule = require("./CommandModule").CommandModule;
 describe("Command Module", () => {
     let commandModule;
     
-    function setup() {
+    beforeEach(() => {
         let grid = new SquareGrid(10);
         let rover = new Rover(new Coords(0, 0), 0);
         commandModule = new CommandModule(grid, rover);
-    }
+    });
 
     it("should return position correctly", () => {
-        setup();
         commandModule.executePath("");
         expect(commandModule.rover.reportLocation()).toEqual("0:0:N");
     });
 
-    it("should move north", () => {
-        setup();
-        commandModule.executePath("M");
-        expect(commandModule.rover.reportLocation()).toEqual("0:1:N");
-        
-
-        setup();
-        commandModule.executePath("MMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:3:N");
-        
-
-        setup();
-        commandModule.executePath("MMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:5:N");
+    it.each([
+        ["M", "0:1:N"],
+        ["MMM", "0:3:N"],
+        ["MMMMM", "0:5:N"],
+    ])( "should move north (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should wrap around", () => {
-        setup();
-        commandModule.executePath("MMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:N");
-
-        setup();
-        commandModule.executePath("MMMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:2:N");
+    it.each([
+        ["MMMMMMMMMM", "0:0:N"],
+        ["MMMMMMMMMMMM", "0:2:N"],
+    ])("should wrap around (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should turn right", () => {
-        setup();
-        commandModule.executePath("R");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:E");
-
-        setup();
-        commandModule.executePath("RR");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:S");
-
-        setup();
-        commandModule.executePath("RRR");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:W");
-
-        setup();
-        commandModule.executePath("RRRR");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:N");
-
-        setup();
-        commandModule.executePath("RRRRR");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:E");
+    it.each([
+        ["R", "0:0:E"],
+        ["RR", "0:0:S"],
+        ["RRR", "0:0:W"],
+        ["RRRR", "0:0:N"],
+        ["RRRRR", "0:0:E"],
+    ])("should turn right (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should turn left", () => {
-        setup();
-        commandModule.executePath("L");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:W");
-
-        setup();
-        commandModule.executePath("LL");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:S");
-
-        setup();
-        commandModule.executePath("LLL");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:E");
-
-        setup();
-        commandModule.executePath("LLLL");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:N");
-
-        setup();
-        commandModule.executePath("LLLLL");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:W");
-    })
-    it("should move east", () => {
-        setup();
-        commandModule.executePath("RM");
-        expect(commandModule.rover.reportLocation()).toEqual("1:0:E");
-
-        setup();
-        commandModule.executePath("RMM");
-        expect(commandModule.rover.reportLocation()).toEqual("2:0:E");
-
-        setup();
-        commandModule.executePath("RMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("5:0:E");
+    it.each([
+        ["L", "0:0:W"],
+        ["LL", "0:0:S"],
+        ["LLL", "0:0:E"],
+        ["LLLL", "0:0:N"],
+        ["LLLLL", "0:0:W"],
+    ])("should turn left (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should wrap around right side of world", () => {
-        setup();
-        commandModule.executePath("RMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:E");
-
-        setup();
-        commandModule.executePath("RMMMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("2:0:E");
+    it.each([
+        ["RM", "1:0:E"],
+        ["RMM", "2:0:E"],
+        ["RMMMMM", "5:0:E"],
+    ])("should move east (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should move south", () => {
-        setup();
-        commandModule.executePath("RRM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:9:S");
-
-        setup();
-        commandModule.executePath("RRMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:7:S");
-
-        setup();
-        commandModule.executePath("RRMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:S");
-
-        setup();
-        commandModule.executePath("RRMMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:9:S");
+    it.each([
+        ["RMMMMMMMMMM", "0:0:E"],
+        ["RMMMMMMMMMMMM", "2:0:E"],
+    ])("should wrap around right side of world (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should move west", () => {
-        setup();
-        commandModule.executePath("LM");
-        expect(commandModule.rover.reportLocation()).toEqual("9:0:W");
-        
-        setup();
-        commandModule.executePath("LMM");
-        expect(commandModule.rover.reportLocation()).toEqual("8:0:W");
-        
-        setup();
-        commandModule.executePath("LMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("5:0:W");
+    it.each([
+        ["RRM", "0:9:S"],
+        ["RRMMM", "0:7:S"],
+        ["RRMMMMMMMMMM", "0:0:S"],
+        ["RRMMMMMMMMMMM", "0:9:S"],
+    ])("should move south (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
-    it("should wrap around left side of world", () => {
-        setup();
-        commandModule.executePath("LMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("0:0:W");
+    it.each([
+        ["LM", "9:0:W"],
+        ["LMM", "8:0:W"],
+        ["LMMMMM", "5:0:W"],
+    ])("should move west (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
+    });
 
-        setup();
-        commandModule.executePath("LMMMMMMMMMMMM");
-        expect(commandModule.rover.reportLocation()).toEqual("8:0:W");
+    it.each([
+        ["LMMMMMMMMMM", "0:0:W"],
+        ["LMMMMMMMMMMMM", "8:0:W"],
+    ])("should wrap around left side of world (%p => %p)", (input, expected) => {
+        commandModule.executePath(input);
+        expect(commandModule.rover.reportLocation()).toEqual(expected);
     });
 
     it("given a grid with no obstacles, input MMRMMLM gives output 2:3:N", () => {
-        setup();
         commandModule.executePath("MMRMMLM");
         expect(commandModule.rover.reportLocation()).toEqual("2:3:N");
     });
@@ -170,7 +116,7 @@ describe("Command Module", () => {
 });
 
 describe("Acceptance tests", () => {
-    it("given a grid with no obstacles, input MMRMMLM gives output 2:3:N", () => {
+    test.each("given a grid with no obstacles, input MMRMMLM gives output 2:3:N", () => {
         let grid = new SquareGrid(10);
         let rover = new Rover(new Coords(0, 0), 0);
         const commandModule = new CommandModule(grid, rover);
